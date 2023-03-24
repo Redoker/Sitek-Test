f862124279aeb8cd7355e68de1b098a1137ef6c5
Redoker <enderdj324@gmail.com>
2023-03-24 18:59:38 +0400

    Теперь код создаёт Excel файл и заносит туда данные. Плюс небольшие правки.

--- Sitek/Program.cs
+++ Sitek/Program.cs
@@ -2,26 +2,25 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Text.Json;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Sitek
-{
-    internal class Program
+{   
+    //объект API
+    public class CusApi
     {
-        //объект API
-        public class CusApi
-        {
-            public string region { get; set; }
-            public string name { get; set; }
-            public string code { get; set; }
-            public string type { get; set; }
-        }
+        public string region { get; set; }
+        public string name { get; set; }
+        public string code { get; set; }
+        public string type { get; set; }
+    }
 
-        public class Root
-        {
-            public List<CusApi> cus { get; set; }
-        }
+    public class Root
+    {
+        public List<CusApi> cus { get; set; }
+    }
 
+    public class Program
+    {
         static void Main(string[] args)
         {
             Root jsonResponse;
@@ -54,16 +53,16 @@ namespace Sitek
 
             //через linq делаем сортировку и проходим по каждому элементу
             //если регион элемента равен 18, то выводим его данные
-            foreach (var item in jsonResponse.cus.OrderBy(x => x.type).ThenBy(x => x.code))
+            /*foreach (var item in jsonResponse.cus.OrderBy(x => x.type).ThenBy(x => x.code))
             {
                 if (item.region == "18")
                 {
                     Console.WriteLine($"{item.type}, {item.code}, {item.name}, {item.region}");
                 }
-            }
+            }*/
 
             ExcelWorksheet excelWorksheet = new ExcelWorksheet();
-            excelWorksheet.CreateFile();
+            excelWorksheet.CreateFile(jsonResponse);
         }
     }
 }
--- Sitek/WriteInExcel.cs
+++ Sitek/WriteInExcel.cs
@@ -1,24 +1,56 @@
-﻿using Microsoft.VisualBasic;
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static Sitek.Program;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Sitek
 {
     public class ExcelWorksheet
     {
-        public void CreateFile()
+        public void CreateFile(Root json)
         {
-            var filepath = "Контролируещие органы.xlsx";
+            var curDir = Environment.CurrentDirectory;
+            var filename = string.Format(@"\Контролирующие органы.xlsx");
+            var filepath = (curDir + filename);
+
             var excelApp = new Excel.Application();
             var workbook = excelApp.Workbooks.Add();
-            var sheet = workbook.ActiveSheet as ExcelWorksheet;
+            Excel._Worksheet worksheet = excelApp.ActiveSheet;
 
-            workbook.SaveAs(filepath);
+            worksheet.Cells[1, "A"] = "Номер";
+            worksheet.Cells[1, "B"] = "Тип";
+            worksheet.Cells[1, "C"] = "Код";
+            worksheet.Cells[1, "D"] = "Название";
+            worksheet.Cells[1, "E"] = "Регион";
+
+            var dataTable = new System.Data.DataTable();
+
+            int row = 2;
+
+            foreach (var item in json.cus.OrderBy(x => x.type).ThenBy(x => x.code))
+            {
+                if (item.region == "18")
+                {
+                    worksheet.Cells[row, 1] = $"{row}";
+                    worksheet.Cells[row, 2] = (string)item.type;
+                    worksheet.Cells[row, 3] = (string)item.code;
+                    worksheet.Cells[row, 4] = (string)item.name;
+                    worksheet.Cells[row, 5] = (string)item.region;
+                    row++;
+                }
+            }
+
+            Console.WriteLine("\n" + "Excel был создан по пути: " + filepath);
+
+            excelApp.DisplayAlerts = false;
+            worksheet.SaveAs(filepath);
             workbook.Close();
             excelApp.Quit();
         }
